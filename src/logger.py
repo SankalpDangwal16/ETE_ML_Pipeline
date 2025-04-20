1180450c5f764c157aabfdccd6f03d0015385d4d
Sankalp Dangwal <sankalp.dangwal@gmail.com>
2025-04-20 15:59:14 +1000

    EDA and model training files

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,6 @@
-import logging
+import sys
+from src.logger import logging 
 import os
-
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+#All evaluation methods will be made here
